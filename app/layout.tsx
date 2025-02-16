faf7214bc1459bc11a704f702e47e868eb0e56f3
Vinay <101291613+Ayush4221@users.noreply.github.com>
2025-02-16 12:01:45 +0530

    changes in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,20 @@
 import { ThemeProvider } from "./providers/theme-provider";
 import "./globals.css";
 import Head from "next/head";
+export const metadata = {
+  manifest: "/manifest.json",
+  name: "HMS",
+  short_name: "HMS",
+  description: "Hostel APP",
+  start_url: "/",
+  display: "standalone",
+};
 
+export function generateViewport() {
+  return {
+    themeColor: "#111827",
+  };
+}
 export default function RootLayout({
   children,
 }: {
